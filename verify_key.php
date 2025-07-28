27b5ab82c825e540333efa028420bbbc23b7699e
alabod1 <safwanalabod@gmail.com>
2025-07-28 20:08:06 +0300

    verify_key.php

--- verify_key.php
+++ verify_key.php
@@ -14,7 +14,7 @@ if (empty($username) || empty($password)) {
 
 // Hardcoded accounts with expiration
 $accounts = [
-    "RetiredGamer" => ["password" => "Gamer123", "expires" => "2025-12-31 23:59:59"],
+    "ab" => ["password" => "1", "expires" => "2025-12-31 23:59:59"],
     "FreeUser"     => ["password" => "FreeKey",   "expires" => "2024-12-31 00:00:00"],
     "BetaTest"     => ["password" => "TryMe",     "expires" => "2025-07-15 12:00:00"]
 ];
